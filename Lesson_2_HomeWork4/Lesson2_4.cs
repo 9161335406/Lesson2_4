00a1e1bcbf1b147be582a00944c6879f45afddab
geekbrains <4091709@mail.ru>
2022-03-07 11:30:15 +0300

    Добавьте файлы проекта.

---
+++ Lesson_2_HomeWork4/Lesson2_4.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lesson_2_HomeWork4
+{
+    class Lesson2_4
+    {
+        public static void Main(string[] args)
+        {
+            string name = "OOO ЭЛИПС";
+            string value = "МАГАЗИН";
+            
+            Console.WriteLine("Кассовый чек");
+            Console.WriteLine($"{name}");
+            Console.WriteLine("Наличными  3000");
+            Console.WriteLine("Сумма без НДС - 3000");
+            Console.WriteLine("СНО: УСН доход-расход____ПРИХОД");
+            Console.WriteLine("СТ .КАССИР - Смыков О.С.");
+            Console.WriteLine("г.Москва");
+            Console.WriteLine($"МЕСТО РАСЧЁТОВ-{value}");
+            Console.WriteLine();
+            Console.WriteLine("СПАСИБО");
+
+            Console.ReadKey();
+        }
+    }
+}
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
